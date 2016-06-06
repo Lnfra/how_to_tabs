aad6b235dce7ce52964a4239ebfe3b4cda5b5b8e
Angeline Tan <atan009@gmail.com>
2016-06-07 04:56:48 +0800

    Using jake for our build

---
+++ Jakefile.js
@@ -0,0 +1,14 @@
+/**
+ * Created by lnfra on 7/6/16.
+ */
+
+(function(){
+    "use strict";
+
+
+    desc("Default Build")
+    task("default", function(){
+        console.log("\n\nBUILD OK");
+    })
+
+}());
