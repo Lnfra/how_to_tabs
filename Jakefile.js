c5db219e80bfeb06ad0ad328e792ccd4c2bd3387
Angeline Tan <atan009@gmail.com>
2016-06-08 00:58:24 +0800

    Added check for node version in build file

--- Jakefile.js
+++ Jakefile.js
@@ -5,10 +5,20 @@
 (function(){
     "use strict";
 
-
+    var EXPECTED_NODE_VERSION = "v5.9.1";
     desc("Default Build")
-    task("default", function(){
+    task("default", [ "version" ], function(){
         console.log("\n\nBUILD OK");
     })
+
+    desc("Check Node version");
+    task("version", function() {
+        console.log("Checking Node version: .");
+
+        var actualVersion = process.version;
+        if(actualVersion !== EXPECTED_NODE_VERSION) {
+            fail("Incorrect Node version: expected " + EXPECTED_NODE_VERSION + ", but was " + actualVersion);
+        }
+    });
 
 }());
