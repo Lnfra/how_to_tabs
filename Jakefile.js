e6efe981717f01cd8b156d940feb947eb79b1ba0
Angeline Tan <atan009@gmail.com>
2016-06-08 01:06:40 +0800

    Build script gets expected node version from package.json

--- Jakefile.js
+++ Jakefile.js
@@ -5,7 +5,6 @@
 (function(){
     "use strict";
 
-    var EXPECTED_NODE_VERSION = "v5.9.1";
     desc("Default Build")
     task("default", [ "version" ], function(){
         console.log("\n\nBUILD OK");
@@ -14,10 +13,11 @@
     desc("Check Node version");
     task("version", function() {
         console.log("Checking Node version: .");
-
+        var packageJson = require("./package.json");
+        var expectedVersion = "v" + packageJson.engines.node;
         var actualVersion = process.version;
-        if(actualVersion !== EXPECTED_NODE_VERSION) {
-            fail("Incorrect Node version: expected " + EXPECTED_NODE_VERSION + ", but was " + actualVersion);
+        if(actualVersion !== expectedVersion) {
+            fail("Incorrect Node version: expected " + expectedVersion + ", but was " + actualVersion);
         }
     });
 
