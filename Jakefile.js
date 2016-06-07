4b7e5eaa4ca28ff333687136c93c87e953c06062
Angeline Tan <atan009@gmail.com>
2016-06-08 03:55:27 +0800

    Enable jshint options

--- Jakefile.js
+++ Jakefile.js
@@ -1,6 +1,7 @@
 /**
  * Created by lnfra on 7/6/16.
  */
+/* globals desc: false, task: false, complete: false, fail: false */
 
 (function(){
     "use strict";
@@ -30,10 +31,33 @@
 
         jshint.checkFiles({
             files: "Jakefile.js",
-            options: {},
-            globals: {}
+            options: {
+                bitwise: true,
+                eqeqeq: true,
+                forin: true,
+                freeze: true,
+                futurehostile: true,
+                latedef: "nofunc",
+                noarg: true,
+                nocomma: true,
+                nonbsp: true,
+                nonew: true,
+                strict: true,
+                undef: true,
+
+                node: true,
+                browser: true
+            },
+            globals: {
+
+            }
         }, complete, fail);
 
     }, {async: true});
+
+    function deleteMe() {
+
+    }
+
 
 }());
