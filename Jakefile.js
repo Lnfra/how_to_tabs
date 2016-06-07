32c4f2dc3f8051223bbd51aafeae4a12319abe72
Angeline Tan <atan009@gmail.com>
2016-06-08 04:19:05 +0800

    Can now use 'jake run' to run a localhost server

--- Jakefile.js
+++ Jakefile.js
@@ -1,7 +1,7 @@
 /**
  * Created by lnfra on 7/6/16.
  */
-/* globals desc: false, task: false, complete: false, fail: false */
+/* globals desc: false, task: false, complete: false, fail: false, jake: false */
 
 (function(){
     "use strict";
@@ -9,10 +9,20 @@
     var semver = require("semver");
     var jshint = require("simplebuild-jshint");
 
+    //****** General-purpose tasks
+
     desc("Default Build");
     task("default", [ "version", "lint" ], function(){
         console.log("\n\nBUILD OK");
     });
+
+
+    desc("Run a localhost server");
+    task("run", function() {
+        jake.exec("node node_modules/http-server/bin/http-server src", {interactive: true}, complete);
+        console.log("Run http-server here");
+    }, {async: true});
+    //****** Supporting tasks
 
     desc("Check Node version");
     task("version", function() {
